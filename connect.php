50ebbcac48742965d51dbe4385fc89acc71160a0
De-Julia <j.denkewicz@gmail.com>
2022-11-29 13:52:26 +0100

    commit 6

--- connect.php
+++ connect.php
@@ -1,4 +1,17 @@
-﻿<?php
+﻿<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Medyczne_Bazy_Danych</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" 
+    crossorigin="anonymous">
+  </head>
+  <body>
+    <a href="http://127.0.0.1:3000/kafelki.html" class="submit btn btn-primary">Powrót do strony głównej</a>
+        <button class="btn btn-primary" type="submit">Zatwierdź formluarz</button>
+<?php
     //Database connection
     $db = mysqli_connect("localhost", "test-user123", "User!234", "klinika_db");
     if (mysqli_connect_errno()){
@@ -23,3 +36,5 @@
     mysqli_query($db, $query2);
     echo "Rejestracja przebiegła pomyślnie :)";
 ?>
+</body>
+</html>
